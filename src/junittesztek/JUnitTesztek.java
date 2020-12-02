93cbdcce5aaeb967d98e816b863aed19126c4639
admin <admin@admin-pc>
2020-12-03 00:00:30 +0100

    ok

--- src/junittesztek/JUnitTesztek.java
+++ src/junittesztek/JUnitTesztek.java
@@ -12,14 +12,20 @@ package junittesztek;
 public class JUnitTesztek {
 public double osszeAd(double a, double b){
         return a+b;
-    }
+    }// Author: Inántsy-Pap Tamás
+
+public double kivonas(double c, double d) {
+        return c-d;
+    }// Author: Pethő Tibor
+
+public double osztas(double e, double f) {
+        return e/f;
+    }// Author: Barcsay Mark
 
     public static void main(String[] args) {
         System.out.println("5.2+6.8 = "+(5.2+6.8));
     }
-    /**
-     * @param args the command line arguments
-     */
+    
 
     
 }
