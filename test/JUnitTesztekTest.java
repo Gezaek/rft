4693954413526323e80870cbc7a3380281739000
admin <admin@admin-pc>
2020-12-02 23:58:16 +0100

    ok

--- test/JUnitTesztekTest.java
+++ test/JUnitTesztekTest.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import junittesztek.JUnitTesztek;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -12,10 +6,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author admin
- */
+
 public class JUnitTesztekTest {
     
    @Test
@@ -27,9 +18,9 @@ public class JUnitTesztekTest {
         double expResult = -9.5;
         double result = instance.osszeAd(a, b);
         assertEquals(expResult, result, 0.0);
-    } 
+    } // Author: Inántsy-Pap Tamás, AWFRNE
     
-    // Author: Inántsy-Pap Tamás, AWFRNE
+    
     @Test
     public void testOsszeAd2() {
         System.out.println("osszeAd2");
@@ -39,5 +30,52 @@ public class JUnitTesztekTest {
         double expResult = a + b;
         double result = instance.osszeAd(a, b);
         assertEquals(expResult, result, 0.0);
-    } 
+    }  // Author: Inántsy-Pap Tamás, AWFRNE
+   
+    
+    
+    @Test
+    public void testKivonas1() {
+        System.out.println("kivonas1");
+        double c = 20;
+        double d = 12;
+        JUnitTesztek instance = new JUnitTesztek();
+        double expResult = c-d;
+        double result = instance.kivonas(c, d);
+        assertEquals(expResult, result, 8);
+    }// Author: Pethő Tibor
+    
+    @Test
+    public void testKivonas2() {
+        System.out.println("kivonas2");
+        double c = 22;
+        double d = 12;
+        JUnitTesztek instance = new JUnitTesztek();
+        double expResult = c-d;
+        double result = instance.kivonas(c, d);
+        assertEquals(expResult, result, 10);
+    } // Author: Pethő Tibor
+    
+    @Test
+    public void testOsztas1() {
+        System.out.println("osztaas1");
+        double e = 12;
+        double f = 6;
+        JUnitTesztek instance = new JUnitTesztek();
+        double expResult = e/f;
+        double result = instance.osztas(e, f);
+        assertEquals(expResult, result, 2);
+    } // Author: Barcsay Mark
+    
+        @Test
+    public void testOsztas2() {
+        System.out.println("osztaas2");
+        double e = 100.0;
+        double f = 5.0;
+        JUnitTesztek instance = new JUnitTesztek();
+        double expResult = e/f;
+        double result = instance.osztas(e, f);
+        assertEquals(expResult, result, 20);
+      
+    } // Author: Barcsay Mark
 }
